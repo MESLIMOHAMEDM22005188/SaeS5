1b40c8c4e73d270c8e359598a73080102c3def4a
MESLIMOHAMEDM22005188 <125298121+MESLIMOHAMEDM22005188@users.noreply.github.com>
2024-11-14 16:18:41 +0100

    amelioration de l'architecture + style login, register + urls

--- mousey/views.py
+++ mousey/views.py
@@ -1,6 +1,7 @@
 # mousey/views.py
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login as auth_login
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib import messages
 
 
@@ -15,3 +16,23 @@ def register(request):
         form = UserCreationForm()
 
     return render(request, 'register.html', {'form': form})
+
+
+def login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                auth_login(request, user)
+                messages.success(request, f'Bienvenue {username} ! Vous êtes connecté.')
+                return redirect('home')  # Remplacez 'home' par la page vers laquelle vous souhaitez rediriger après la connexion
+            else:
+                messages.error(request, 'Identifiants invalides. Veuillez réessayer.')
+        else:
+            messages.error(request, 'Identifiants invalides. Veuillez réessayer.')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form})
