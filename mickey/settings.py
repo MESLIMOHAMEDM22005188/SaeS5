e7422b750ecb937ac6b7053c006fbeccc27876eb
MESLIMOHAMEDM22005188 <125298121+MESLIMOHAMEDM22005188@users.noreply.github.com>
2024-11-21 15:24:07 +0100

    test sécu

--- mickey/settings.py
+++ mickey/settings.py
@@ -5,6 +5,7 @@ from pathlib import Path
 CSRF_TRUSTED_ORIGINS = [
     'https://saes5.onrender.com',
 ]
+AUTH_USER_MODEL = 'mousey.CustomUser'
 
 # Base directory
 BASE_DIR = Path(__file__).resolve().parent.parent
--- mousey/models.py
+++ mousey/models.py
@@ -2,8 +2,6 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
-class CustomUser(AbstractUser):
-    phone_number = models.CharField(max_length=15, unique=True, null=True, blank=True)
 
-    def __str__(self):
-        return self.username
+class CustomUser(AbstractUser):
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
