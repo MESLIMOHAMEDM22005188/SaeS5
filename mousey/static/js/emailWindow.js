efdc3797214edf77bd748c870718ce88ea232f9f
MESLIMOHAMEDM22005188 <125298121+MESLIMOHAMEDM22005188@users.noreply.github.com>
2024-11-23 10:24:01 +0100

    emailWindow v2.1

--- mousey/static/js/emailWindow.js
+++ mousey/static/js/emailWindow.js
@@ -1,31 +1,14 @@
-//liste des emails
 const emails = [
-    {
-        id: 1,
-        sender: "ruuqn44Jrf@gmiel.com",
-        subject: "Gagnez une tablette Apple !",
-        body: "Félicitations ! Vous avez été sélectionné parmis 100000 candidats pour gagner une tablette Apple. Cliquez ici pour retirer votre gain : <a href='http://win-free-apple-not-v1ru5.co'>http://win-free-apple-not-v1ru5.co</a>",
-    },
-    {
-        id: 2,
-        sender: "secretaire-cybermouse@gmiel.com",
-        subject: "Attention : Problème de sécurité",
-        body: "Bonjour,<br><br>De nombreux élèves ont été victimes de phishing récemment. Veuillez redoubler de vigilence et évitez au maximum les liens suspects.<br><br>Merci, la secrétaire."
-    },
-    {
-        id: 3,
-        sender: "secreta1re-cybermouse@gmiel.com",
-        subject: "Formation : Apprendre a détecter le phishing",
-        body: "Bonjour,<br><br>Voici le lien pour accéder à une formation sur le phishing : <a href='http://formation-phishing-1.com'>http://formation-phishing-1.com</a>"
-    }
-]
+    { id: 1, sender: "ruuqn44Jrf@gmiel.com", subject: "Gagnez une tablette Apple !", body: "Félicitations ! Vous avez été sélectionné parmi 100000 candidats pour gagner une tablette Apple. Cliquez ici pour retirer votre gain : <a href='http://win-free-apple-not-v1ru5.co'>http://win-free-apple-not-v1ru5.co</a>" },
+    { id: 2, sender: "secretaire-cybermouse@gmiel.com", subject: "Attention : Problème de sécurité", body: "Bonjour,<br><br>De nombreux élèves ont été victimes de phishing récemment. Veuillez redoubler de vigilance et évitez les liens suspects.<br><br>Merci, la secrétaire." },
+    { id: 3, sender: "secreta1re-cybermouse@gmiel.com", subject: "Formation : Apprendre à détecter le phishing", body: "Bonjour,<br><br>Voici le lien pour accéder à une formation sur le phishing : <a href='http://formation-phishing-1.com'>http://formation-phishing-1.com</a>" }
+];
 
 function openEmailWindow() {
     document.getElementById('email-window').style.display = 'block';
     loadEmailTitles();
 }
 
-//femer fenêtre
 document.getElementById('close-email-window').addEventListener('click', () => {
     document.getElementById('email-window').style.display = 'none';
 });
@@ -42,7 +25,6 @@ function loadEmailTitles() {
     });
 }
 
-// Afficher le contenu d'un email
 function displayEmailContent(emailId) {
     const email = emails.find(e => e.id === emailId);
     const emailContent = document.getElementById('email-content');
