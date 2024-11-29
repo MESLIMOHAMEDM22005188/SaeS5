46977836c00997cd5c4c99ef3f7dee3142cca1e8
MESLIMOHAMEDM22005188 <125298121+MESLIMOHAMEDM22005188@users.noreply.github.com>
2024-11-29 10:25:42 +0100

    prevention

--- mousey/static/js/emailWindow.js
+++ mousey/static/js/emailWindow.js
@@ -7,8 +7,18 @@ document.getElementById("close-email-window").addEventListener("click", function
     emailWindow.style.display = "none";
 });
 const emails = [
-    { subject: "...", content: "Bonjour, merci d'avoir choisi Outlook." },
-    { subject: "Mise à jour de votre compte", content: "Votre compte a été mis à jour avec succès." },
+    {
+        subject: "Urgent : Mise à jour obligatoire de votre compte RH",
+        content: "Bonjour,\n\nVotre compte RH nécessite une mise à jour immédiate pour se conformer aux nouvelles réglementations. Veuillez cliquer sur le lien ci-dessous pour valider vos informations :\n\n[http://faux-site-rh.com/mise-a-jour]\n\nSi vous ne le faites pas sous 24 heures, votre compte sera suspendu.\n\nCordialement,\nÉquipe RH"
+    },
+    {
+        subject: "Mise à jour de votre profil RH",
+        content: "Bonjour [Nom du collaborateur],\n\nDans le cadre de la mise à jour de nos données internes, nous vous invitons à vérifier et, si nécessaire, à actualiser vos informations personnelles via votre portail RH.\n\nVous pouvez accéder à votre compte en toute sécurité à l'adresse suivante :\n[https://intranet.entreprise.com/moncompte]\n\nSi vous avez des questions ou rencontrez des problèmes, n'hésitez pas à nous contacter à cette adresse : rh@entreprise.com.\n\nCordialement,\n[Nom et prénom]\nResponsable RH\n[Entreprise]"
+    },
+    {
+        subject: "Félicitations pour votre excellent travail !",
+        content: "Bonjour [Prénom],\n\nJe tiens à vous féliciter pour votre engagement et votre contribution exceptionnelle au projet [Nom du projet]. Votre travail a été remarqué et apprécié par l’ensemble de l’équipe dirigeante.\n\nContinuez sur cette lancée, votre implication est précieuse pour l’entreprise.\n\nCordialement,\n[Nom du Patron]\nDirecteur Général\n[Entreprise]"
+    }
 ];
 
 function loadEmails() {
