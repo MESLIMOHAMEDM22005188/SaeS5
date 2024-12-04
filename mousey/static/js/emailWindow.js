89998ab88bda55392031bf925b429b3b8ad959b5
MESLIMOHAMEDM22005188 <125298121+MESLIMOHAMEDM22005188@users.noreply.github.com>
2024-12-04 22:50:12 +0100

    commit final ajout du bouton de Jeu

--- mousey/static/js/emailWindow.js
+++ mousey/static/js/emailWindow.js
@@ -1,7 +1,8 @@
-function openEmailWindow() {
+document.getElementById("open-email-window").addEventListener("click", function () {
     const emailWindow = document.getElementById("email-window");
     emailWindow.style.display = "block";
-}
+});
+
 document.getElementById("close-email-window").addEventListener("click", function () {
     const emailWindow = document.getElementById("email-window");
     emailWindow.style.display = "none";
@@ -10,25 +11,15 @@ document.getElementById("close-email-window").addEventListener("click", function
 const emails = [
     {
         subject: "Urgent : Mise à jour obligatoire de votre compte RH",
-        content: "Bonjour,\n\nVotre compte RH nécessite une mise à jour immédiate pour se conformer aux
-        "nouvelles réglementations. Veuillez cliquer sur le lien ci-dessous pour valider vos informations
-        ":\n\n[http://faux-site-rh.com/mise-a-jour]\n\nSi vous ne le faites pas sous 24 heures, votre compte sera suspendu.
-        "\n\nCordialement,\nÉquipe RH"
+        content: "Bonjour,\n\nVotre compte RH nécessite une mise à jour immédiate..."
     },
     {
         subject: "Mise à jour de votre profil RH",
-        content: "Bonjour monsieur Sanchez,\n\nDans le cadre de la mise à jour de nos données internes,
-        "nous vous invitons à vérifier et, si nécessaire, à actualiser vos informations personnelles via votre portail RH.
-        "\n\nVous pouvez accéder à votre compte en toute sécurité à l'adresse suivante :
-        "\n[https://intranet.entreprise.com/moncompte]\n\nSi vous avez des questions ou rencontrez des problèmes,
-        "n'hésitez pas à nous contacter à cette adresse : rh@entreprise.com.\n\nCordialement,\n[Nom et prénom]\nResponsable RH\n[Entreprise]"
+        content: "Bonjour monsieur Sanchez,\n\nDans le cadre de la mise à jour..."
     },
     {
         subject: "Félicitations pour votre excellent travail !",
-        content: "Bonjour Rick Sanchez,\n\nJe tiens à vous féliciter pour votre engagement et votre contribution exceptionnelle
-        "au projet NavalTravel. Votre travail a été remarqué et apprécié par l’ensemble de l’équipe dirigeante.\n
-        "\nContinuez sur cette lancée, votre implication est précieuse pour l’entreprise.\n\nCordialement,\n Alexis Delin\n
-        "Directeur Général\n LeviatorGroup"
+        content: "Bonjour Rick Sanchez,\n\nJe tiens à vous féliciter pour votre engagement..."
     }
 ];
 
