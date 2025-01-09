f5c262dad3f7e873d59df8e6bf6f94b214473dd6
MESLIMOHAMEDM22005188 <125298121+MESLIMOHAMEDM22005188@users.noreply.github.com>
2025-01-09 14:06:46 +0100

    fenetre google

---
+++ mousey/static/js/googleWindow.js
@@ -0,0 +1,9 @@
+function openWhiteBox() {
+    const whiteBox = document.getElementById('white-box');
+    whiteBox.style.display = 'block'; // Afficher le carré
+}
+
+function closeWhiteBox() {
+    const whiteBox = document.getElementById('white-box');
+    whiteBox.style.display = 'none'; // Cacher le carré
+}
