c59db6bbecbd8048018b925096d1f42102765bc8
MESLIMOHAMEDM22005188 <125298121+MESLIMOHAMEDM22005188@users.noreply.github.com>
2025-02-02 17:26:09 +0100

    déploiment 2

--- manage.py
+++ manage.py
@@ -1,14 +1,19 @@
+#!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
 
 
 def main():
+    # Configure le module de réglages Django
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mickey.settings')
+
+    # Récupère le port depuis la variable d'environnement PORT (par défaut "10000")
     port = os.environ.get('PORT', '10000')
 
     from django.core.management import execute_from_command_line
 
+    # Si aucun argument n'est passé (autre que le nom du script), lance runserver sur 0.0.0.0:<port>
     if len(sys.argv) == 1:
         sys.argv.extend(['runserver', f'0.0.0.0:{port}'])
 
